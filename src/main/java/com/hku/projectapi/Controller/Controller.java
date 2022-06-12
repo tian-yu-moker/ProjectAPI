4114c8d036277bc6e7c22f3231fd6ca8818517a3
Tian Yu <17722024@bjtu.edu.cn>
2022-06-12 15:47:02 +0800

    V1.7 Token implemented

--- src/main/java/com/hku/projectapi/Controller/Controller.java
+++ src/main/java/com/hku/projectapi/Controller/Controller.java
@@ -1,14 +1,19 @@
 package com.hku.projectapi.Controller;
 
 
+import com.hku.projectapi.Tools.JwtUtil;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+
 
 @CrossOrigin(origins = {"*","null"})
 @RestController
@@ -37,5 +42,26 @@ public class Controller
         List<Map<String, Object>> info = jdbcTemplate.queryForList(sql);
         System.out.println(info);
         return "Tian Yu";
+    }
+
+    @GetMapping("/author/hello")
+    public String hello() {
+        return "hello world";
+    }
+
+    @GetMapping("/auth/login")
+    public String login(String username, String pass) {
+        //假设数据库中查询到了该用户，这里测试先所及生成一个UUID，作为用户的id
+        String userId = UUID.randomUUID().toString();
+
+        //准备存放在IWT中的自定义数据
+        Map<String, Object> info = new HashMap<>();
+        info.put("username", "tom");
+        info.put("pass", "admin");
+
+        //生成JWT字符串
+        String token = JwtUtil.sign(userId, info);
+
+        return token;
     }
 }
