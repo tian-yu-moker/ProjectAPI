336ce91d9b7a041eb493e68f0d6b6bd694c022c4
Tian Yu <17722024@bjtu.edu.cn>
2022-06-17 10:58:36 +0800

    Service added

--- src/main/java/com/hku/projectapi/Controller/CollectionController.java
+++ src/main/java/com/hku/projectapi/Controller/CollectionController.java
@@ -3,14 +3,19 @@ package com.hku.projectapi.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.hku.projectapi.Beans.Result;
+import com.hku.projectapi.Service.CollectionLikeService;
 
 @CrossOrigin(origins = {"*","null"})
 @RestController
 public class CollectionController 
 {
+    @Autowired
+    private CollectionLikeService collections;
+
     @GetMapping("getUsersLike")
     public Object getAllLike()
     {
+
         return "";
     }
 }
--- src/main/java/com/hku/projectapi/Service/CollectionLikeService.java
+++ src/main/java/com/hku/projectapi/Service/CollectionLikeService.java
@@ -1,9 +1,18 @@
 package com.hku.projectapi.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.hku.projectapi.Mapper.KnowledgeLikeMapper;
 
 @Service
 public class CollectionLikeService 
 {
-    
+    @Autowired
+    private KnowledgeLikeMapper knowledgeMapper;
+
+    public void getKnowledge()
+    {
+        
+    }
 }
