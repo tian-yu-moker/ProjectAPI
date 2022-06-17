7322eba04cd77f630020996c86fac9acefa514eb
Tian Yu <17722024@bjtu.edu.cn>
2022-06-17 10:41:53 +0800

    v 2.7 knowledge like

---
+++ src/main/java/com/hku/projectapi/Mapper/KnowledgeLikeMapper.java
@@ -0,0 +1,13 @@
+package com.hku.projectapi.Mapper;
+
+import com.hku.projectapi.Beans.knowledge_like;
+
+import org.springframework.stereotype.Repository;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+@Repository
+public interface KnowledgeLikeMapper extends BaseMapper<knowledge_like>
+{
+    
+}
