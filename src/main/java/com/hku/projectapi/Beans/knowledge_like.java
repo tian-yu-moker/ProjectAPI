39b5034fabf77c7e3da3fed5915735c67de24744
Tian Yu <17722024@bjtu.edu.cn>
2022-06-17 10:53:10 +0800

    first commit

---
+++ src/main/java/com/hku/projectapi/Beans/knowledge_like.java
@@ -0,0 +1,10 @@
+package com.hku.projectapi.Beans;
+import lombok.Data;
+
+@Data
+public class knowledge_like
+{
+    private String email;
+    private String knowledge_id;
+    private String add_date;
+}
