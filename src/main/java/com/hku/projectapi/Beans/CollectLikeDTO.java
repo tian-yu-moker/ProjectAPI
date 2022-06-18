81c7618168a6da9f1c8e285679918700af3efc9e
Tian Yu <17722024@bjtu.edu.cn>
2022-06-18 16:09:22 +0800

    mapper test

---
+++ src/main/java/com/hku/projectapi/Beans/CollectLikeDTO.java
@@ -0,0 +1,11 @@
+package com.hku.projectapi.Beans;
+
+import lombok.Data;
+
+@Data
+public class CollectLikeDTO 
+{
+    private String email;
+    private String knowledge_id;
+    private int type;
+}
