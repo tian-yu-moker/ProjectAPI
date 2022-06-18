65c947921d1d27cc8e8f93bc2b423e0efb2180e2
Tian Yu <17722024@bjtu.edu.cn>
2022-06-18 17:09:21 +0800

    query & create

--- src/main/java/com/hku/projectapi/Controller/CollectionController.java
+++ src/main/java/com/hku/projectapi/Controller/CollectionController.java
@@ -12,8 +12,8 @@ import com.hku.projectapi.Service.CollectionLikeService;
 @RestController
 public class CollectionController 
 {
-//    @Autowired
-//    private CollectionLikeService collectionService;
+    @Autowired
+    private CollectionLikeService collectionService;
 
     /**
      * Add a liked question
@@ -23,37 +23,73 @@ public class CollectionController
     @PostMapping("/users_like")
     public Object addLike(@RequestBody CollectLikeDTO dto, @RequestHeader("token") String token)
     {
-        // try{
-        //     if(dto.getType() == 0)
-        //     {
-        //         int resCode = collectionService.addLikeKnowledge(dto.getEmail(), dto.getKnowledge_id());
-        //         if(resCode == 0)
-        //         {
-        //             return new Result<Object>("00", "Success.", token, null);
-        //         }
-        //         else
-        //         {
-        //             return new Result<Object>("09", "Already liked such question.", token, null);
-        //         }
-        //     }
-        //     else
-        //     {
+         try{
+             if(dto.getType() == 0)
+             {
+                 int resCode = collectionService.addLikeKnowledge(dto.getEmail(), dto.getKnowledge_id());
+                 if(resCode == 0)
+                 {
+                     return new Result<Object>("00", "Success.", token, null);
+                 }
+                 else
+                 {
+                     return new Result<Object>("09", "Already liked such question.", token, null);
+                 }
+             }
+             // For interview questions
+             else
+             {
 
-        //     }
-        // } catch(Exception exception)
-        // {
-        //     return new Result<Object>("99", "Internal service error.", token, null);
-        // }
+             }
+         } catch(Exception exception)
+         {
+             return new Result<Object>("99", "Internal service error.", token, null);
+         }
         String res = "";
         return res;
     }
 
+    /**
+     *
+     * @param email users' email
+     * @param knowledge_id optional, knowledge id
+     * @param inter_ques_id optional, interview question id
+     * @param interview_id optional, interview id
+     * @return
+     */
     @GetMapping("/users_like")
-    public Object getAllLike()
+    public Object getAllLike(// @RequestHeader("token") String token,
+                             @RequestParam(value = "email", required = false) String email,
+                             @RequestParam(value = "knowledge_id", required = false) String knowledge_id,
+                             @RequestParam(value = "inter_ques_id", required = false) String inter_ques_id,
+                             @RequestParam(value = "interview_id", required = false) String interview_id)
     {
-        String result = "";
+        System.out.println(email + " " + knowledge_id + " " + inter_ques_id);
+        // If search all collections for one user
+        if(knowledge_id.equals(null) && inter_ques_id.equals(null) && interview_id.equals(null))
+        {
+            System.out.println("all");
+        }
+        // Search for knowledge
+        else if(!knowledge_id.equals(null) && inter_ques_id.equals(null) && interview_id.equals(null))
+        {
+            System.out.println("knowledge");
+        }
+        // Search for interview question
+        else if(knowledge_id.equals(null) && !inter_ques_id.equals(null) && interview_id.equals(null))
+        {
+            System.out.println("interview question");
+        }
+        // Search for whole interview
+        else if(knowledge_id.equals(null) && inter_ques_id.equals(null) && !interview_id.equals(null))
+        {
+            System.out.println("interview");
+        }
+        String result = "Get request.";
         return result;
     }
+
+
 
     @DeleteMapping("/users_like")
     public Object cancelLike()
--- src/main/java/com/hku/projectapi/Service/CollectionLikeService.java
+++ src/main/java/com/hku/projectapi/Service/CollectionLikeService.java
@@ -19,6 +19,7 @@ import com.hku.projectapi.Mapper.KnowledgeLikeMapper;
 
 import com.hku.projectapi.Beans.Result;
 
+@Service
 @SpringBootTest
 public class CollectionLikeService 
 {
@@ -44,36 +45,41 @@ public class CollectionLikeService
 
     }
 
-//    /**
-//     * Create a liked knowledge question, if no record, create one, if yes, return error.
-//     * @param email
-//     * @param knowledgeId
-//     */
-//    public int addLikeKnowledge(String email, String knowledgeId)
-//    {
-//        QueryWrapper<knowledge_like> oneQuery = new QueryWrapper<>();
-//        oneQuery.eq(!email.equals(null), "email", email).
-//        eq(!knowledgeId.equals(null), "knowledge_id", knowledgeId);
-//        List<knowledge_like> res = knowledgeMapper.selectList(oneQuery);
-//        if(res.size() != 0)
-//        {
-//            knowledge_like likeBean = new knowledge_like(email, knowledgeId, new Timestamp(new Date().getTime()));
-//            knowledgeMapper.insert(likeBean);
-//            return 0;
-//        }
-//        return -1;
-//    }
+    /**
+     * Create a liked knowledge question, if no record, create one, if yes, return error.
+     * @param email
+     * @param knowledgeId
+     */
+    public int addLikeKnowledge(String email, String knowledgeId)
+    {
+        QueryWrapper<knowledge_like> oneQuery = new QueryWrapper<>();
+        oneQuery.eq(!email.equals(null), "email", email).
+        eq(!knowledgeId.equals(null), "knowledge_id", knowledgeId);
+        List<knowledge_like> res = knowledgeMapper.selectList(oneQuery);
+        // If no such record, then create one.
+        if(res.size() == 0)
+        {
+            knowledge_like likeBean = new knowledge_like(email, knowledgeId, new Timestamp(new Date().getTime()));
+            knowledgeMapper.insert(likeBean);
+            return 0;
+        }
+        return -1;
+    }
 
     @Test
     public void test()
     {
         String email = "123@qq.com";
-        String knowledgeId = "4186f6f46bb94450ae1b3abe54517229";
+        String knowledgeId = "4186f6f46bb94450ae1b3abe54517228";
         QueryWrapper<knowledge_like> oneQuery = new QueryWrapper<>();
         oneQuery.eq("email", email).
                 eq("knowledge_id", knowledgeId);
         List<knowledge_like> res = knowledgeMapper.selectList(oneQuery);
-        System.out.println(res);
+        if(res.size() == 0)
+        {
+            knowledge_like likeBean = new knowledge_like(email, knowledgeId, new Timestamp(new Date().getTime()));
+            knowledgeMapper.insert(likeBean);
+        }
 
     }
 
