215cf04f43161271843af66da6c1e80240e30853
tian-yu-moker <17722024@bjtu.edu.cn>
2022-07-13 18:14:16 +0800

    Programming initial

--- src/main/java/com/hku/projectapi/Beans/Programming/ProgrammingQuestionBean.java
+++ src/main/java/com/hku/projectapi/Beans/Programming/ProgrammingQuestionBean.java
@@ -4,9 +4,13 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @TableName("programming_questions")
 public class ProgrammingQuestionBean
 {
