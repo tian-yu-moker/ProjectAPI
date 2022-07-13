44914d30776d5e046ca86675fe1ea561c03e23d5
tian-yu-moker <17722024@bjtu.edu.cn>
2022-07-13 18:21:21 +0800

    Test

---
+++ src/main/java/com/hku/projectapi/Programming/Util/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.hku.projectapi.Programming.Util;
+
+import java.util.*;
+import java.math.*;
+class Solution {
+    public int climbStairs(int n) {
+        //solution
+        return 1;
+    }
+}
+public class HelloWorld{
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        for(int i = 0; i < 10; i++){
+            int arg = Integer.parseInt(args[i]);
+            System.out.println(solution.climbStairs(arg));
+        }
+    }
+}
+
