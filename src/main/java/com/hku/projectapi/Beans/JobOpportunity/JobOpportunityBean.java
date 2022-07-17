3a05d9253683a03e95ddf0262f343fecf443454d
Tian Yu <17722024@bjtu.edu.cn>
2022-07-17 21:57:25 +0800

    Job bean

--- src/main/java/com/hku/projectapi/Beans/JobOpportunity/JobOpportunityBean.java
+++ src/main/java/com/hku/projectapi/Beans/JobOpportunity/JobOpportunityBean.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,4 +16,35 @@ public class JobOpportunityBean
 {
     @TableField("uuid")
     private String uuid;
+
+    @TableField("company")
+    private String company;
+
+    @TableField("job_position")
+    private String jobPosition;
+
+    @TableField("job_location")
+    private String jobLocation;
+
+    @TableField("job_description_short")
+    private String descriptionShort;
+
+    @TableField("job_description_full")
+    private String descriptionFull;
+
+    @TableField("salary")
+    private String salary;
+
+    @TableField("hr_contract")
+    private String hrContract;
+
+    @TableField("upload_time")
+    private Timestamp uploadTime;
+
+    @TableField("expiry_time")
+    private Timestamp expiryTime;
+
+    // need to disable the card (not allowed to be clicked)
+    @TableField(exist = false)
+    private boolean isExpired;
 }
--- src/main/java/com/hku/projectapi/Service/JobOpportunity/JobOpportunityService.java
+++ src/main/java/com/hku/projectapi/Service/JobOpportunity/JobOpportunityService.java
@@ -3,10 +3,27 @@ package com.hku.projectapi.Service.JobOpportunity;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hku.projectapi.Beans.JobOpportunity.JobOpportunityBean;
 import com.hku.projectapi.Mapper.JobOpportunity.JobOpportunityMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JobOpportunityService extends ServiceImpl<JobOpportunityMapper, JobOpportunityBean>
 {
+    @Autowired
+    private JobOpportunityMapper jobOpportunityMapper;
 
+    public void create()
+    {
+
+    }
+
+    public void searchByPage()
+    {
+
+    }
+
+    public void searchById()
+    {
+
+    }
 }
