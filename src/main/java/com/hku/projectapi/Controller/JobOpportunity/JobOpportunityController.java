6ad27fcbc9309c52fda476b794c2ade0eb0c3211
tian-yu-moker <17722024@bjtu.edu.cn>
2022-07-18 10:56:40 +0800

    job create, load interface

--- src/main/java/com/hku/projectapi/Controller/JobOpportunity/JobOpportunityController.java
+++ src/main/java/com/hku/projectapi/Controller/JobOpportunity/JobOpportunityController.java
@@ -1,8 +1,11 @@
 package com.hku.projectapi.Controller.JobOpportunity;
 
+import com.hku.projectapi.Beans.JobOpportunity.JobOpportunityBean;
 import com.hku.projectapi.Beans.QueryByPageDTO;
 import com.hku.projectapi.Beans.Result;
+import com.hku.projectapi.Service.JobOpportunity.JobOpportunityService;
 import com.hku.projectapi.Tools.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {"*","null"})
@@ -10,12 +13,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/job_service")
 public class JobOpportunityController
 {
+    @Autowired
+    private JobOpportunityService jobOpportunityService;
+
     @PostMapping("create")
-    public Result create(@RequestHeader String token)
+    public Result create(@RequestHeader String token, @RequestBody JobOpportunityBean jobOpportunityBean)
     {
         String updatedToken = "";
         try {
             updatedToken = JwtUtil.updateToken(token);
+            jobOpportunityService.create(jobOpportunityBean);
+            return new Result("00", "Success.", updatedToken);
         }catch (Exception e){
             return new Result("97", "Invalid token, please login.", null);
         }
@@ -28,6 +36,7 @@ public class JobOpportunityController
         String updatedToken = "";
         try {
             updatedToken = JwtUtil.updateToken(token);
+            return new Result("00", "Success.", updatedToken);
         }catch (Exception e){
             return new Result("97", "Invalid token, please login.", null);
         }
@@ -39,6 +48,7 @@ public class JobOpportunityController
         String updatedToken = "";
         try {
             updatedToken = JwtUtil.updateToken(token);
+            return new Result("00", "Success.", updatedToken);
         }catch (Exception e){
             return new Result("97", "Invalid token, please login.", null);
         }
--- src/main/java/com/hku/projectapi/Service/JobOpportunity/JobOpportunityService.java
+++ src/main/java/com/hku/projectapi/Service/JobOpportunity/JobOpportunityService.java
@@ -1,10 +1,21 @@
 package com.hku.projectapi.Service.JobOpportunity;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hku.projectapi.Beans.JobOpportunity.JobOpportunityBean;
+import com.hku.projectapi.Beans.PageRequestDTO;
+import com.hku.projectapi.Beans.QueryByPageDTO;
+import com.hku.projectapi.Beans.QueryInfo;
+import com.hku.projectapi.Beans.Result;
 import com.hku.projectapi.Mapper.JobOpportunity.JobOpportunityMapper;
+import com.hku.projectapi.Tools.UUidGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class JobOpportunityService extends ServiceImpl<JobOpportunityMapper, JobOpportunityBean>
@@ -12,14 +23,40 @@ public class JobOpportunityService extends ServiceImpl<JobOpportunityMapper, Job
     @Autowired
     private JobOpportunityMapper jobOpportunityMapper;
 
-    public void create()
+    public void create(JobOpportunityBean jobOpportunityDTO)
     {
-
+        String uuid = UUidGenerator.getUUID32();
+        jobOpportunityDTO.setUuid(uuid);
+        jobOpportunityMapper.insert(jobOpportunityDTO);
     }
 
-    public void searchByPage()
+    public Result searchByPage(PageRequestDTO pageRequestDTO, String token)
     {
+        int curPage= pageRequestDTO.getPageFirst();
+        int pageSize = pageRequestDTO.getPageSizeFirst();
+        QueryWrapper<JobOpportunityBean> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select().orderByDesc("upload_time");
+        Page<JobOpportunityBean> resPage = jobOpportunityMapper.selectPage(new Page<>(curPage, pageSize), queryWrapper);
+        List<JobOpportunityBean> records = resPage.getRecords();
 
+        for(JobOpportunityBean beans:records){
+            Timestamp expiry = beans.getExpiryTime();
+            Timestamp curTime = new Timestamp(new Date().getTime());
+            if(expiry.before(curTime)){
+                beans.setExpired(true);
+            }else{
+                beans.setExpired(false);
+            }
+        }
+
+        QueryByPageDTO result = new QueryByPageDTO();
+        QueryInfo queryInfo = new QueryInfo();
+        queryInfo.setPageSize(pageRequestDTO.getPageSizeFirst());
+        queryInfo.setCurrentPage(pageRequestDTO.getPageFirst());
+        queryInfo.setTotalRecord(resPage.getTotal());
+        result.setQueryInfo(queryInfo);
+        result.setEntities(records);
+        return new Result("00", "Success.", null, result);
     }
 
     public void searchById()
